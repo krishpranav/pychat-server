e777cf79d6860f0a25d9b6dbff0582fdc364fa8e
Krisna Pranav <krisna.pranav@gmail.com>
2022-05-02 17:30:07 +0530

    server: basic server functionality

--- server/server.py
+++ server/server.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+import socket
+import select
+
+'''
+    - IP + Port
+'''
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 8080
+
+'''
+    - Server socket functions
+'''
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {} # clients should be blank to allow many clients
+
+# print out the msg to inform the user that server is running
+print(f'Listening for incoming connections on {IP}:{PORT}....') 
